5a5cea310111165cfddd53e5bf97ea4c40b33719
shaneram0 <shaneram@usf.edu>
2026-04-22 14:36:05 -0400

    Calculate weekly pay based on user input
    Convert user input for work hours to float and calculate weekly pay.

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ print("Welcome to Shane's USF Python Assignment!")
 
 work_hours = input("How many hours did you work this week? ")
 #User input for hours worked
+
+work_hours = float(work_hours)
+weekly_pay = work_hours * 16.50
+#Calculating weekly pay based on user input
